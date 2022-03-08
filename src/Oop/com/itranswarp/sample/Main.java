929b530d44f0125da2cd461501fbc6db6668aeaf
liuyu <liuy@ggxx.net>
2022-03-08 15:02:07 +0800

    gitsync

---
+++ src/Oop/com/itranswarp/sample/Main.java
@@ -0,0 +1,15 @@
+package Oop.com.itranswarp.sample;
+
+/**
+ * Learn Java from https://www.liaoxuefeng.com/
+ * 
+ * @author liaoxuefeng
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Person p1 = new Person("xiao ming");
+		System.out.println(p1.hello());
+	}
+
+}
---
+++ src/Oop/com/itranswarp/sample/Person.java
@@ -0,0 +1,14 @@
+package Oop.com.itranswarp.sample;
+
+public class Person {
+
+	String name;
+
+	public Person(String name) {
+		this.name = name;
+	}
+
+	public String hello() {
+		return "Hello, " + this.name;
+	}
+}
